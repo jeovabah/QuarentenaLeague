a13b80a9eda9ba6f9c33ae1f0404ae84904eb259
jeova <jeova.junior@me2.com.br>
2022-11-15 21:52:15 -0300

    retirando moral

--- src/components/CardTeam/index.tsx
+++ src/components/CardTeam/index.tsx
@@ -1,65 +1,67 @@
-import { Avatar, Box, Text } from "@chakra-ui/react";
-import { useEffect } from "react";
+import { Avatar, Box, Modal, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 interface Props {
   title: string;
   description: string;
   players: any;
 }
 export const CardTeam = ({ title, description, players }: Props) => {
+  const [modal, setModal] = useState(false);
   return (
     <>
-      <Text
-        fontWeight={600}
-        color={"white"}
-        w="100%"
-        borderBottom="1px solid #d3d3d3"
-        textAlign={"center"}
-      >
-        {title}
-      </Text>
-      <Box
-        w={"100%"}
-        p={"20px 10px"}
-        background={"#202024"}
-        borderRadius={"10px"}
-        borderBottom={"1px solid var(--primary)"}
-        display={"flex"}
-        alignItems={"center"}
-        justifyContent={"center"}
-      >
+      <Box onClick={() => setModal(!modal)}>
+        <Text
+          fontWeight={600}
+          color={"white"}
+          w="100%"
+          borderBottom="1px solid #d3d3d3"
+          textAlign={"center"}
+        >
+          {title}
+        </Text>
         <Box
+          w={"100%"}
+          p={"20px 10px"}
+          background={"#202024"}
+          borderRadius={"10px"}
+          borderBottom={"1px solid var(--primary)"}
           display={"flex"}
-          flexDir={"column"}
           alignItems={"center"}
           justifyContent={"center"}
-          gap={"1rem"}
-          maxH={"280px"}
-          w={"100%"}
         >
-          {players &&
-            players.map((player: any, index: number) => (
-              <Text
-                key={index}
-                color={"GrayText"}
-                w="100%"
-                display={"flex"}
-                alignItems={"center"}
-                justifyContent={"space-between"}
-                textTransform={"capitalize"}
-                fontWeight={600}
-              >
-                {player.player}
-                <Avatar
-                  size={"md"}
+          <Box
+            display={"flex"}
+            flexDir={"column"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            gap={"1rem"}
+            maxH={"280px"}
+            w={"100%"}
+          >
+            {players &&
+              players.map((player: any, index: number) => (
+                <Text
                   key={index}
-                  name={player.name}
-                  src={player.link_url}
-                />
-              </Text>
-            ))}
-        </Box>
-        <Box display={"flex"} gap={"0.5rem"}>
-          {/* {players &&
+                  color={"GrayText"}
+                  w="100%"
+                  display={"flex"}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}
+                  textTransform={"capitalize"}
+                  fontWeight={600}
+                >
+                  {player.player}
+                  <Avatar
+                    size={"md"}
+                    key={index}
+                    name={player.name}
+                    src={player.link_url}
+                  />
+                </Text>
+              ))}
+          </Box>
+          <Box display={"flex"} gap={"0.5rem"}>
+            {/* {players &&
           players.map((player: any, index: number) => (
             <Avatar
               key={index}
@@ -68,6 +70,7 @@ export const CardTeam = ({ title, description, players }: Props) => {
               src={player.link_url}
             />
           ))} */}
+          </Box>
         </Box>
       </Box>
     </>
