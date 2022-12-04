4e99761b231841bc5d6c41a60df0d0a4f67b77fa
Jeova Junior <jrjeova@hotmail.com>
2022-12-04 00:26:16 -0300

    resolvendo problemas

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -47,6 +47,34 @@ export default function RegisterPlayer() {
       alert("Preencha todos os campos");
     }
   }
+
+  const [players, setPlayers] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  async function readPlayerData() {
+    setLoading(true);
+    await get(ref(database, "player/")).then((snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        const teams = Object.entries(data).map(([key, value]: any) => {
+          return {
+            player: value.player,
+            position: value.position,
+            link_url: value.link_url,
+            team: value.team,
+          };
+        });
+        console.log(teams);
+        setPlayers(teams);
+      } else {
+        console.log("No data available");
+      }
+      setLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    readPlayerData();
+  }, []);
   async function writeTeamData(team: string) {
     // increment id
     if (team !== "") {
@@ -79,6 +107,8 @@ export default function RegisterPlayer() {
   useEffect(() => {
     readTeamData();
   }, []);
+
+  const updateLinkUrl = (e: any) => {};
   //transform first object in array
   // const teams = Object.keys(teams).map((key) => teams[key]);
   // console.log(teams);
@@ -172,6 +202,58 @@ export default function RegisterPlayer() {
           >
             Registrar Jogador
           </Button>
+        </Box>
+        <Box mt="4rem">
+          <Header title="Editar Jogador" />
+
+          <Select
+            onChange={(e: any) =>
+              setPlayer({ ...player, team: e.target.value })
+            }
+            color="white"
+            backgroundColor={"#202024"}
+            value={player.team}
+            size="lg"
+            border={"none"}
+            mt="1rem"
+            mb="1rem"
+          >
+            <option
+              value=""
+              style={{
+                color: "white",
+                backgroundColor: "#202024",
+              }}
+            >
+              Selecione um Jogador
+            </option>
+            {players.length > 0 &&
+              players.map((team: any) => (
+                <option
+                  value={team.player}
+                  style={{
+                    color: "white",
+                    backgroundColor: "#202024",
+                  }}
+                  key={team.player}
+                >
+                  {team.player}
+                </option>
+              ))}
+          </Select>
+          <Input
+            placeholder="Link Da Imagem"
+            onChange={(e: any) =>
+              setPlayer({ ...player, link_url: e.target.value })
+            }
+            value={team}
+            size="lg"
+          />
+          <Box mt="1rem" w={"100%"}>
+            <Button w={"100%"} size="lg" onClick={() => writeTeamData(team)}>
+              Editar
+            </Button>
+          </Box>
         </Box>
         <Box mt="5rem">
           <Input
